c363a2ae7446f4a09178446c95ff36c1047e65de
himanshu <himanshu.2776@gmail.com>
2018-10-07 23:21:31 +0530

    initial commit :himanshu pal

---
+++ basicauth/src/main/java/basicauth/Upwork.java
@@ -0,0 +1,15 @@
+package basicauth;
+
+public class Upwork {
+	public static void main(String[] args) {
+		
+
+	try{
+		System.out.println("1");
+		throw new Exception();
+	}
+	catch(Exception ex){
+		
+	}
+	}
+}
